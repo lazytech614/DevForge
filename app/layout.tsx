30ac5fc4b344bbc2c58c107b67fa358ddfdf46f0
lazytech614 <derupanjan2021@gmail.com>
2026-06-22 14:48:33 +0530

    feat: implement dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "DevForge",
@@ -16,15 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-
-        <main className="container mx-auto min-h-screen px-4 py-10">
-          {children}
-        </main>
-
-        <Footer />
-      </body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body>
+          <Navbar />
+          <main className="container mx-auto min-h-screen px-4 py-10">
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
