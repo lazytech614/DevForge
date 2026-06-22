9b694356b72e93f30143f3f8e75d408118f97e88
lazytech614 <derupanjan2021@gmail.com>
2026-06-22 14:44:00 +0530

    update: chnage the title of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 
 export const metadata: Metadata = {
-  title: "Backend Playground",
+  title: "DevForge",
   description: "Developer tools and visualizers",
 };
 
